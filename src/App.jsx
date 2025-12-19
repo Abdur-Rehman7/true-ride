c961fc2af5092b3e83d09164d9c4c68c404354ba
Abdur-Rehman7 <reactsolution786@gmail.com>
2025-12-19 22:51:46 +0500

    adding project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-900">
+        <h1 className="text-4xl font-bold text-white">
+          Tailwind is working 🚀
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
